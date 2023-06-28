f26f303a690273d908a113148046828ec51658d5
zudo <44712315+zudo@users.noreply.github.com>
2023-06-28 15:07:16 +0200

    input output

--- transaction/src/lib.rs
+++ transaction/src/lib.rs
@@ -5,81 +5,60 @@ use serde::Serialize;
 use serde_big_array::BigArray;
 use sha2::Digest;
 use sha2::Sha256;
-use vint::vint;
 use vint::Vint;
-#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
-pub struct Transaction {
-    pub output_address: [u8; 20],
-    pub amount: Vint<4>,
-    pub fee: Vint<4>,
-    pub timestamp: u32,
+#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct Input {
+    pub output: ([u8; 32], u8),
     #[serde(with = "BigArray")]
     pub signature: [u8; 64],
 }
+impl Input {
+    pub fn hash(&self) -> [u8; 32] {
+        let mut hasher = Sha256::new();
+        hasher.update(&self.output.0);
+        hasher.update(&self.output.1.to_be_bytes());
+        hasher.finalize().into()
+    }
+}
+#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct Output {
+    pub address: [u8; 20],
+    pub value: Vint<4>,
+}
+impl Output {
+    pub fn hash(&self) -> [u8; 32] {
+        let mut hasher = Sha256::new();
+        hasher.update(&self.address);
+        hasher.update(self.value.0);
+        hasher.finalize().into()
+    }
+}
+#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct Transaction {
+    pub inputs: Vec<Input>,
+    pub outputs: Vec<Output>,
+}
 impl Transaction {
-    pub fn sign(
-        output_address: [u8; 20],
-        amount: u128,
-        fee: u128,
-        timestamp: u32,
-        key: &Key,
-    ) -> Result<Transaction, Error> {
+    pub fn sign(mut inputs: Vec<(Input, Key)>, outputs: Vec<Output>) -> Result<Transaction, Error> {
         let mut transaction = Transaction {
-            output_address,
-            amount: vint!(amount, 4),
-            fee: vint!(fee, 4),
-            timestamp,
-            signature: [0; 64],
+            inputs: inputs.iter().map(|x| x.0.clone()).collect::<Vec<_>>(),
+            outputs,
         };
-        transaction.signature = key.sign(&transaction.hash())?;
+        let hash = transaction.hash();
+        for (input, key) in &mut inputs {
+            input.signature = key.sign(&hash)?;
+        }
+        transaction.inputs = inputs.into_iter().map(|x| x.0).collect::<Vec<_>>();
         Ok(transaction)
     }
     pub fn hash(&self) -> [u8; 32] {
-        let mut array = [0; 32];
-        array[0..20].copy_from_slice(&self.output_address);
-        array[20..24].copy_from_slice(&self.timestamp.to_be_bytes());
-        array[24..28].copy_from_slice(&self.amount.0);
-        array[28..32].copy_from_slice(&self.fee.0);
         let mut hasher = Sha256::new();
-        hasher.update(array);
-        hasher.finalize().into()
-    }
-    pub fn input_address(&self) -> Result<[u8; 20], Error> {
-        Ok(Key::address(&self.input_public_key()?))
-    }
-    pub fn input_public_key(&self) -> Result<[u8; 33], Error> {
-        Key::recover(&self.hash(), &self.signature)
-    }
-}
-impl Default for Transaction {
-    fn default() -> Transaction {
-        Transaction {
-            output_address: [0; 20],
-            amount: Vint([0; 4]),
-            fee: Vint([0; 4]),
-            timestamp: 0,
-            signature: [0; 64],
+        for input in &self.inputs {
+            hasher.update(&input.hash());
         }
-    }
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn hash() {
-        assert_eq!(
-            Transaction::default().hash(),
-            [
-                102, 104, 122, 173, 248, 98, 189, 119, 108, 143, 193, 139, 142, 159, 142, 32, 8,
-                151, 20, 133, 110, 226, 51, 179, 144, 42, 89, 29, 13, 95, 41, 37
-            ]
-        );
-    }
-    #[test]
-    fn bincode_serialize() {
-        assert_eq!(
-            bincode::serialize(&Transaction::default()).unwrap().len(),
-            96
-        );
+        for output in &self.outputs {
+            hasher.update(&output.hash());
+        }
+        hasher.finalize().into()
     }
 }
